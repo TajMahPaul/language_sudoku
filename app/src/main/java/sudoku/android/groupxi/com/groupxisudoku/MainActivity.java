e45fe07ce0235d96c45e6e51bc4bbca68b134f74
Jacky Lee <jacky_lee_5@sfu.ca>
2019-01-29 00:04:24 -0800

    Trying to fix merge conflicts

--- app/src/main/java/sudoku/android/groupxi/com/groupxisudoku/MainActivity.java
+++ app/src/main/java/sudoku/android/groupxi/com/groupxisudoku/MainActivity.java
@@ -1,25 +1,30 @@
 package sudoku.android.groupxi.com.groupxisudoku;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button playButton;
+    private final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        playButton = (Button) findViewById(R.id.playButton);
-        playButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this, GameActivity.class);
-                startActivity(intent);
-            }
-        });
     }
+
+    // play
+    public void playButton(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+
+    // settings
+    public void settingsButton(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
 }
