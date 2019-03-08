4e07ee10395c115b1ec075a0141f06921bae306a
Ziyuan Ma <ziyuanma@Ziyuans-MacBook-Pro.local>
2019-03-07 22:24:26 -0800

    replace fragment layout with gridview

--- app/src/main/java/sudoku/android/groupxi/com/groupxisudoku/GameActivity.java
+++ app/src/main/java/sudoku/android/groupxi/com/groupxisudoku/GameActivity.java
@@ -1,18 +1,12 @@
 package sudoku.android.groupxi.com.groupxisudoku;
 
-import android.content.Intent;
 import android.content.res.Resources;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Spinner;
-import android.widget.TextView;
+import android.widget.GridView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
@@ -24,21 +18,17 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-import sudoku.android.groupxi.com.groupxisudoku.fragments.CellGroupFragment;
 import sudoku.android.groupxi.com.groupxisudoku.model.Board;
-import sudoku.android.groupxi.com.groupxisudoku.model.Pair;
 
 
-
-public class GameActivity extends AppCompatActivity implements CellGroupFragment.OnFragmentInteractionListener {
+public class GameActivity extends AppCompatActivity {
     private final String TAG = "GameActivity";
-    private TextView clickedCell = null;
-    private int clickedGroup = 0;
-    private int clickedCellId = 0;
     public Board startBoard;
     public Board currentBoard;
     private Button[] num_buttons = new Button [9];
-    public int swap = 0;
+    public int language = 0; // 0 for native and 1 for chinese on board
+    List<Integer> boardNumber = new ArrayList<>();
+    GridViewAdapter adapter;
     //test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,46 +38,8 @@ public class GameActivity extends AppCompatActivity implements CellGroupFragment
         Resources res = getResources();
         String[] native_strings = res.getStringArray(R.array.native_array);
         String[] chinese_strings = res.getStringArray(R.array.chinese_array);
+        GridView gridView = findViewById(R.id.gridView);
 
-
-        //Spinner logic
-        /*Spinner spinner = (Spinner) findViewById(R.id.language_spinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.array_languages, R.layout.spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-                int Buttons[] = new int[]{R.id.num_button1, R.id.num_button2, R.id.num_button3, R.id.num_button4,
-                        R.id.num_button5, R.id.num_button6, R.id.num_button7, R.id.num_button8, R.id.num_button9};
-
-                Resources res = getResources();
-
-                if (position == 0){
-                    String[] language_strings = res.getStringArray(R.array.chinese_array);
-                    for (int i = 0; i < 9; i++) {
-                        Button button = (Button)findViewById(Buttons[i]);
-                        button.setText(language_strings[i]);
-                    }
-                }else if (position == 1){
-                    String[] language_strings = res.getStringArray(R.array.spanish_array);
-                    for (int i = 0; i < 9; i++) {
-                        Button button = (Button)findViewById(Buttons[i]);
-                        button.setText(language_strings[i]);
-                    }
-
-                }
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parentView) {
-                // your code here
-            }
-
-        });*/
 
         int difficulty = getIntent().getIntExtra("difficulty", 0);
         ArrayList<Board> boards = readGameBoards(difficulty);
@@ -95,33 +47,16 @@ public class GameActivity extends AppCompatActivity implements CellGroupFragment
         currentBoard = new Board();
         currentBoard.copyValues(startBoard.getGameCells());
 
-        int cellGroupFragments[] = new int[]{R.id.cellGroupFragment, R.id.cellGroupFragment2, R.id.cellGroupFragment3, R.id.cellGroupFragment4,
-                R.id.cellGroupFragment5, R.id.cellGroupFragment6, R.id.cellGroupFragment7, R.id.cellGroupFragment8, R.id.cellGroupFragment9};
-        for (int i = 1; i < 10; i++) {
-            CellGroupFragment thisCellGroupFragment = (CellGroupFragment) getSupportFragmentManager().findFragmentById(cellGroupFragments[i-1]);
-            thisCellGroupFragment.setGroupId(i);
-        }
-
         //Appear all values from the current board
-        CellGroupFragment tempCellGroupFragment;
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                int column = j / 3;
-                int row = i / 3;
-
-                int fragmentNumber = (row * 3) + column;
-                tempCellGroupFragment = (CellGroupFragment) getSupportFragmentManager().findFragmentById(cellGroupFragments[fragmentNumber]);
-                int groupColumn = j % 3;
-                int groupRow = i % 3;
-
-                int groupPosition = (groupRow * 3) + groupColumn;
-                int currentValue = currentBoard.getValue(i, j);
-
-                if (currentValue != 0) {
-                    tempCellGroupFragment.setValue(groupPosition, native_strings[currentValue - 1]);
-                }
+                boardNumber.add(currentBoard.getValue(i, j));
             }
         }
+
+
+        adapter = new GridViewAdapter(boardNumber, native_strings, chinese_strings, language,this);
+        gridView.setAdapter(adapter);
 
         //add function to number buttons
         int numButtonsId[] = new int[]{R.id.num_button1, R.id.num_button2, R.id.num_button3, R.id.num_button4,
@@ -134,19 +69,19 @@ public class GameActivity extends AppCompatActivity implements CellGroupFragment
                 @Override
                 public void onClick(View v) {
                     //check if a cell has been selected
-                    if(clickedGroup != 0){
-                        int row = ((clickedGroup-1)/3)*3 + clickedCellId / 3;
-                        int column = (clickedGroup-1)%3*3 + clickedCellId % 3;
+                    if(adapter.isClicked()){
+                        int row = adapter.getRow();
+                        int column = adapter.getColumn();
                         //check if the number can fill in this cell
                         if(currentBoard.isBoardCorrect(row,column, finalI) == true){
                             currentBoard.setValue(row,column, finalI);
                             Button temp_button = (Button) v;
                             String text = (String) temp_button.getText();
-                            clickedCell.setText(text);
-                            clickedCell.setBackgroundResource(R.drawable.table_border_cell);
-                            clickedCell = null;
-                            clickedGroup = 0;
-                            clickedCellId = 0;
+                            Button clickedButton = adapter.getClickedCell();
+                            clickedButton.setText(text);
+                            adapter.setClicked(false);
+                            adapter.uncheckClickedCell();
+
                         }else{
                             Toast.makeText(GameActivity.this, R.string.board_incorrect, Toast.LENGTH_SHORT).show();
 
@@ -162,6 +97,21 @@ public class GameActivity extends AppCompatActivity implements CellGroupFragment
                 }
             });
         }
+
+        Button deleteButton = findViewById(R.id.delete_button);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(adapter.isClicked()){
+                    Button clickedButton = adapter.getClickedCell();
+                    clickedButton.setText("");
+                    adapter.uncheckClickedCell();
+                    int row = adapter.getRow();
+                    int column = adapter.getColumn();
+                    currentBoard.setValue(row,column, 0);
+                }
+            }
+        });
 
     }
 
@@ -255,60 +205,13 @@ public class GameActivity extends AppCompatActivity implements CellGroupFragment
         return boards.get(randomNumber);
     }
 
-    private boolean isStartPiece(int group, int cell) {
-        int row = ((group-1)/3)*3 + (cell/3);
-        int column = ((group-1)%3)*3 + ((cell)%3);
-        return startBoard.getValue(row, column) != 0;
-    }
-
 
     public void onGoBackButtonClicked() {
         finish();
     }
 
 
-    @Override
-    public void onFragmentInteraction(int groupId, int cellId, View view) {
-        Log.i(TAG, "Clicked group " + groupId + ", cell " + cellId);
-
-        //if there's selected cell previously, reset the background of that cell
-        if(clickedCell != null){
-            clickedCell.setBackgroundResource(R.drawable.table_border_cell);
-        }
-
-        if (!isStartPiece(groupId, cellId)) {
-            clickedCell = (TextView) view;
-            clickedGroup = groupId;
-            clickedCellId = cellId;
-            view.setBackgroundResource(R.drawable.table_border_cell_selected);
-        } else {
-            int row = ((groupId-1)/3)*3 + (cellId/3);
-            int column = ((groupId-1)%3)*3 + ((cellId)%3);
-            int num = currentBoard.getValue(row, column);
-            if(swap == 0){
-                String[] chinese_strings = getResources().getStringArray(R.array.chinese_array);
-                Toast.makeText(this, chinese_strings[num - 1], Toast.LENGTH_SHORT).show();
-            }else{
-                String[] native_strings = getResources().getStringArray(R.array.native_array);
-                Toast.makeText(this, native_strings[num - 1], Toast.LENGTH_SHORT).show();
-
-            }
-            clickedCell = null;
-            clickedGroup = 0;
-            clickedCellId = 0;
-        }
-    }
-
-    public void deleteButton(View view){
-        if(clickedGroup != 0){
-            clickedCell.setText("");
-            int row = ((clickedGroup-1)/3)*3 + clickedCellId / 3;
-            int column = (clickedGroup-1)%3*3 + clickedCellId % 3;
-            currentBoard.setValue(row,column, 0);
-            clickedCell.setBackgroundResource(R.drawable.table_border_cell);
-        }
-    }
-
+    /*
     public void swapButton(View view){
         String[] chinese_strings = getResources().getStringArray(R.array.chinese_array);
         String[] native_strings = getResources().getStringArray(R.array.native_array);
@@ -351,5 +254,5 @@ public class GameActivity extends AppCompatActivity implements CellGroupFragment
             }
             swap = 0;
         }
-    }
+    }*/
 }
