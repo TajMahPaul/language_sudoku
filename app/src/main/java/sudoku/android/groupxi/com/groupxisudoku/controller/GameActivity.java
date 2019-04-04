b957f7b4faa30273ab01f09adb92a597e6cd0599
Jacky Lee <jacky_lee_5@sfu.ca>
2019-04-03 21:06:31 -0700

    last minute changes for demo and goal setting

--- app/src/main/java/sudoku/android/groupxi/com/groupxisudoku/controller/GameActivity.java
+++ app/src/main/java/sudoku/android/groupxi/com/groupxisudoku/controller/GameActivity.java
@@ -235,6 +235,7 @@ public class GameActivity extends AppCompatActivity {
             }
         }
 
+        // initialize buttons
         for(int i = 0; i < size; i++){
             final int finalI = i+1;
             num_buttons[i] = findViewById(numButtonsId[i]);
